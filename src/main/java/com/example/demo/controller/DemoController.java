d2acc0b0ed2ef0ddab851097b0f615504d409313
offcn-java <soulstare89@163.com>
2019-08-16 18:59:52 +0800

    dev开发

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,4 +1,21 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class DemoController {
+
+    @RequestMapping("/selectUsers")
+    public List<String> getList(){
+        List<String> list = new ArrayList<>();
+        for(int i=0;i<10;i++){
+            list.add(i+"abc");
+        }
+        return list;
+    }
 }
