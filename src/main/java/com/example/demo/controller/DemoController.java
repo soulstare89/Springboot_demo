3208192968ab37c99945d985aaa6d6a10fec53e1
offcn-java <soulstare89@163.com>
2019-08-16 19:05:35 +0800

    fix bug

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,4 +1,13 @@
 package com.example.demo.controller;
 
+@RestController
 public class DemoController {
+	 @RequestMapping("/selectUsers")
+	    public List<String> getList(){
+	        List<String> list = new ArrayList<>();
+	        for(int i=0;i<10;i++){
+	            list.add(i+"abc");
+	        }
+	        return list;
+	    }
 }
