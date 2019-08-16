2e19bfd6262eb69afb43367366a14078b552232f
offcn-java <soulstare89@163.com>
2019-08-16 19:29:23 +0800

    eclpse

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
-<<<<<<< HEAD
-=======
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
